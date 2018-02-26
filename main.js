6598ae5801904eaee09eda651a960b564bedc2af
James Mosier <jamesdmosier@gmail.com>
2018-02-25 22:20:40 -0500

    got plex status working on an interval

--- main.js
+++ main.js
@@ -1,68 +1,69 @@
+require('dotenv').config();
+
 const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow, Tray } = electron;
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const path = require('path');
 const url = require('url');
-
 const menubar = require('menubar');
+const PlexAPI = require('plex-api');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+const millisToMins = require('./utils/millis-to-mins');
 
-function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+const plexOptions = {
+  hostname: '127.0.0.1',
+  username: process.env.PLEX_USERNAME,
+  password: process.env.PLEX_PASSWORD,
+};
+const client = new PlexAPI(plexOptions);
+const mb = menubar({ icon: 'static/icon.png' });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true,
-    })
-  );
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-}
-
-function createMenubar() {
-  var mb = menubar();
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createMenubar);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
+mb.on('after-create-window', () => {
+  if (isDevelopment) {
+    mb.window.openDevTools();
   }
 });
 
-app.on('activate', function() {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+mb.on('ready', () => {
+  getSessionStatus();
+
+  setInterval(() => {
+    getSessionStatus();
+  }, 5000); // 60000
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+function toggleIcon(isActive) {
+  console.log('Icon is active?', isActive);
+  const iconName = isActive ? 'icon-active' : 'icon';
+  mb.tray.setImage(`static/${iconName}.png`);
+}
+
+async function getSessionStatus() {
+  try {
+    const resp = await client.query('/status/sessions');
+    console.log(resp);
+    const data = resp.MediaContainer;
+
+    if (!data.size || data.size === 0) {
+      // nothing playing right now
+      toggleIcon(false);
+      return;
+    }
+
+    const runningVideos = data.Video.map((vid) => {
+      return {
+        title: vid.title,
+        type: vid.type,
+        duration: millisToMins(vid.duration),
+        thumb: vid.thumb,
+        art: vid.art,
+      };
+    });
+
+    toggleIcon(true);
+  } catch (e) {
+    console.log('Error getting session status', e);
+  }
+}
